6e8735f5df99b7aaa02012099c66b1809eadf948
Tom Bartindale <tom.bartindale@monash.edu>
2021-02-13 22:09:39 +1100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+router-view
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+/*
+
+Voting / polling data (yes/no) animated results
+
+Displaying content (winners / losers) (winners on content)
+
+Displaying messages?
+
+Control Panel
+
+*/
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+   
+  },
+  data:function(){
+    return {
+      
+    }
+  },
+  
 }
 </script>
 
+<style src="animate.css/animate.css">
+    /* global styles */
+</style> 
+
+<style src="motion-css-animation/motion.css">
+    /* global styles */
+</style> 
+
 <style>
-#app {
+@import url('https://fonts.googleapis.com/css2?family=Hammersmith+One&family=Titillium+Web&display=swap');
+</style> 
+
+<style>
+.widescreen {
+  width:1280px;
+  height:720px;
+  overflow: hidden;
+  position:relative;
+  /* background:#00ff00; */
+  /* background:transparent; */
+}
+
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin:0;
+  padding:0;
+  background:gray;
+}
+
+.fade-in-enter-active {
+  transition: opacity 300ms cubic-bezier(0.55, 0.085, 0.68, 0.53);
+}
+
+.fade-in-leave-active {
+  transition: opacity 225ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
+}
+
+.fade-in-enter,
+.fade-in-leave-to {
+  opacity: 0;
 }
 </style>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,44 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { rtdbPlugin } from 'vuefire'
+import VueRouter from 'vue-router'
+
+
+Vue.use(rtdbPlugin)
+Vue.use(require("vue-moment"));
+Vue.use(VueRouter);
+
+import Display from './components/Display'
+import Control from './components/Control'
+import Dashboard from './components/Dashboard'
+
+
+const routes = [
+  { path: '/display/:user/:id', component: Display, props: true },
+  { path: '/control/:user/:id', component: Control, props: true },
+  { path: '/:id', component: Dashboard, props: true}
+]
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
+import "./styles/quasar.sass";
+import "@quasar/extras/roboto-font/roboto-font.css";
+import "@quasar/extras/material-icons/material-icons.css";
+import Quasar from "quasar";
+
+Vue.use(Quasar, {
+  config: {
+    brand: {
+      primary: "#4082AD",
+      secondary: "#358462",
+    }
+  },
+  extras: ["material-icons"]
+});
+
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+new Vue({router,render: h => h(App)}).$mount('#app')
